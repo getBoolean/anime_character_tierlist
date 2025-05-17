772beebe750440e16e82bab9433620e4bbe43a1d
getBoolean <19920697+getBoolean@users.noreply.github.com>
2025-05-17 15:23:03 -0500

    disable web hash unless on github pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:anime_character_tierlist/src/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  const useHashUrlStrategy = bool.fromEnvironment(
+    'ENABLE_HASH_URL_STRATEGY',
+    defaultValue: false,
+  );
+  if (!useHashUrlStrategy) {
+    usePathUrlStrategy();
+  }
   runApp(ProviderScope(child: MyApp()));
 }
