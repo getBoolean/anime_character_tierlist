991b5fbdb71e942cc95b1bf14ada0b00b6c6cf41
getBoolean <19920697+getBoolean@users.noreply.github.com>
2025-05-17 22:38:05 +0000

    Deploying to gh-pages from @ getBoolean/anime_character_tierlist@8760a1147316fd5558cbe88104656be92003791e 🚀

--- fix/auth/flutter_bootstrap.js
+++ fix/auth/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3003040744"
+    serviceWorkerVersion: "2457393877"
   }
 });
